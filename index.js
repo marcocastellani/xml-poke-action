27826c09e01dd47a5a746590de385c8d68094347
Marco Castellani <marcocastellani@hotmcil.com>
2020-07-23 22:54:41 +0200

    My first action is ready

--- index.js
+++ index.js
@@ -2,12 +2,18 @@ const core = require("@actions/core");
 const github = require("@actions/github");
 const xmlpoke = require("xmlpoke");
 
+const addns = (xml, ns, alias) => {
+  if (ns !== undefined) xml.addNamespace(alias, ns);
+};
+
 try {
   const fileGlob = core.getInput("file-glob");
   const select = core.getInput("select");
   const set = core.getInput("set");
   const clear = core.getInput("clear");
   const remove = core.getInput("remove");
+  const namespace = core.getInput("namespace");
+  const namespaceAlias = core.getInput("namespace-alias");
   console.log(`Processing file ${fileGlob}`);
   console.log(`Search  ${select}`);
   console.log(`Replace ${set}`);
@@ -16,17 +22,20 @@ try {
 
   if (set !== undefined) {
     xmlpoke(fileGlob, function (xml) {
+      addns(xml, namespace, namespaceAlias);
       xml.set("data/connString", "server=oh;db=hai");
     });
   }
 
   if (clear !== undefined) {
+    addns(xml, namespace, namespaceAlias);
     xmlpoke(fileGlob, function (xml) {
       xml.clear(clear);
     });
   }
 
   if (remove !== undefined) {
+    addns(xml, namespace, namespaceAlias);
     xmlpoke(fileGlob, function (xml) {
       xml.remove(remove);
     });
